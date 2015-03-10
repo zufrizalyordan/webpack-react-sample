09845416df9a84c426f625391fbd2e44eb43a21d
zufrizalyordan <zufrizalyordan@gmail.com>
2015-03-10 13:10:19 +0700

    external module

---
+++ Michael.jsx
@@ -0,0 +1,9 @@
+/** @jsx React.DOM */
+'use strict'
+var React = require('react')
+module.exports = React.createClass({
+    displayName: 'MichaelReact',
+    render: function(){
+        return <div>My name is Michael</div>
+    }
+})
