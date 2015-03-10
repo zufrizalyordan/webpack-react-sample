0f4839c86cab7d1df96346bd9e08c3be655e815b
zufrizalyordan <zufrizalyordan@gmail.com>
2015-03-10 11:25:23 +0700

    webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        filename: 'bundle.js', //this is the default name, so you can skip it
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            }
+        ]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
